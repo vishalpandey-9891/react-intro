5107f179ee687ba15a9e7de06556bd93825f6ceb
Vishal kumar Pandey <vishalkumar.pandey@dxc.com>
2020-09-05 21:44:56 +0530

    adding component 2'

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
 
-function App() {
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
+render() {
     return (  <div className="App">
     <Navbar dark color="primary">
       <div className="container">
         <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
       </div>
     </Navbar>
-    <Menu />
-  </div>
-    );
+    <Menu dishes={this.state.dishes} />
+  </div>);}
 }
 
 export default App;
