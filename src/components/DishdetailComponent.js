c02552ad0a4dc586f448495dc5c19fa6eef353db
Vishal kumar Pandey <vishalkumar.pandey@dxc.com>
2020-09-09 21:32:14 +0530

    Assignment task 1

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,7 +1,12 @@
-import React, { Component } from 'react';
-import { Card, CardImg, CardText, CardBody,
-    CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
-import { Link } from 'react-router-dom';
+import React,{Component} from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem,Button, Modal,ModalHeader, 
+	ModalBody, Jumbotron, Form, FormGroup, Input, Label,Col,Row } from 'reactstrap';
+	
+import {Link} from 'react-router-dom';
+import {Control, LocalForm, Errors } from 'react-redux-form';
+
+
+
     function RenderDish({dish}) {
         console.log("DishDetail Component render is invoked");
     	if(dish!= null){
@@ -68,7 +73,8 @@ import { Link } from 'react-router-dom';
                         <RenderDish dish={props.dish} />
                     </div>
                     <div className="col-12 col-md-5 m-1">
-                        <RenderComments comments={props.comments} />
+					<RenderComments comments={props.comments} />
+                        <CommentForm />
                     </div>
                 </div>
                 </div>
@@ -81,7 +87,99 @@ import { Link } from 'react-router-dom';
 
 
 
-    }
+		}
+
+		class CommentForm extends Component{
+		
+		
+			constructor(props){
+				super(props);
+		
+		
+		
+				this.state={
+					isModalOpen: false
+				};
+		
+		
+		
+				this.handleSubmit = this.handleSubmit.bind(this);
+				this.toggleModal = this.toggleModal.bind(this);
+			}
+		
+			handleSubmit(values) {
+				console.log('Current State is: ' + JSON.stringify(values));
+				alert('Current State is: ' + JSON.stringify(values));
+			}
+
+
+			toggleModal(){
+				this.setState({
+					isModalOpen: !this.state.isModalOpen
+				});
+			}
+		
+			render(){
+				return(
+					<div>
+						<Button outline color="secondary"  onClick={this.toggleModal}>
+							<span className="fa fa-pencil fa-lg"></span> Submit Comment
+						</Button>
+						<Modal isOpen={this.state.isModalOpen} toggle = {this.toggleModal}>
+								<ModalHeader toggle = {this.toggleModal}> Submit Comment </ModalHeader>
+								<ModalBody>
+									<LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+										<Row className="form-group">
+											<Label htmlFor="rating" md={2}>Rating</Label>
+											<Col md={12}>
+												<Control.select model=".rating" name="rating"
+												className="form-control">
+												<option>1</option>
+												<option>2</option>
+												<option>3</option>
+												<option>4</option>
+												<option>5</option>
+												<option>6</option>
+												<option>7</option>
+												<option>8</option>
+												<option>9</option>
+												<option>10</option>
+												</Control.select>
+											</Col>
+										</Row>
+										<Row className="form-group">
+											<Label htmlFor="yourName" md={12}>Your Name</Label>
+											<Col md={12}>
+												<Control.text model=".yourName" id="yourName" name="yourName"
+												placeholder="Your Name"
+												className="form-control"
+												 />
+											</Col>
+										</Row>
+										<Row className="form-group">
+											<Label htmlFor="comment" md={12}>Comment</Label>
+											<Col md={12}>
+												<Control.textarea model=".comment" id="comment" name="comment"
+													rows="5"
+													className="form-control" />
+											</Col>
+										</Row>
+										<Row className="form-group">
+											<Col md={{size:10, offset: 2}}>
+												<Button type="submit" color="primary">
+												Submit</Button>
+											</Col>
+										</Row>
+									</LocalForm>
+								</ModalBody>
+						</Modal>
+					</div>
+				);
+		
+			}
+		
+		}
+		
 
 
 export default DishDetail;
