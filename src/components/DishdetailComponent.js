722318ee0af332b30f6061d34837c23dae1f95da
Vishal kumar Pandey <vishalkumar.pandey@dxc.com>
2020-09-09 21:50:51 +0530

    Combining Reducers

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,6 +1,6 @@
 import React,{Component} from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem,Button, Modal,ModalHeader, 
-	ModalBody, Jumbotron, Form, FormGroup, Input, Label,Col,Row } from 'reactstrap';
+ ModalBody, Label,Col,Row } from 'reactstrap';
 	
 import {Link} from 'react-router-dom';
 import {Control, LocalForm, Errors } from 'react-redux-form';
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -27,10 +27,7 @@ const mapStateToProps = state => {
 
 class Main extends Component {
 
-  constructor(props) {
-    super(props);
-
-  }
+ 
 
   onDishSelect(dishId) {
     this.setState({ selectedDish: dishId});
