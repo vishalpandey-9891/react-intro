db3aa42118ca8c9e0c4de551f03df7e0c63de8c4
Vishal kumar Pandey <vishalkumar.pandey@dxc.com>
2020-09-09 20:17:46 +0530

    Single Page Applications Part 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
-
+import { Card, CardImg, CardText, CardBody,
+    CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
     function RenderDish({dish}) {
         console.log("DishDetail Component render is invoked");
     	if(dish!= null){
@@ -49,18 +50,30 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
     const  DishDetail = (props) => {
         if (props.dish != null) {
-        	 return (
-		          <div className="container">
-		            <div className="row">
-		            	<div className="col-12 col-md-5 m-1">
-		            		<RenderDish dish={props.dish} />
-		            	</div>
-		            	<div className="col-12 col-md-5 m-1">
-							<RenderComments comments={props.dish.comments}/>
-		            	</div>
-		            </div>
-		          </div>
-	        );
+			return (
+                <div className="container">
+                <div className="row">
+                    <Breadcrumb>
+
+                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr />
+                    </div>                
+                </div>
+                <div className="row">
+                    <div className="col-12 col-md-5 m-1">
+                        <RenderDish dish={props.dish} />
+                    </div>
+                    <div className="col-12 col-md-5 m-1">
+                        <RenderComments comments={props.comments} />
+                    </div>
+                </div>
+                </div>
+            );
+            
         	}else{
         		return(<div></div>);
         	}
