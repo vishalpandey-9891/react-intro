7beb1861a306c033cdbbd1b1124afe516dded9d8
Vishal kumar Pandey <vishalkumar.pandey@dxc.com>
2020-09-09 21:08:10 +0530

    Intro. to Redux

--- src/App.js
+++ src/App.js
@@ -6,15 +6,21 @@ import Menu from './components/MenuComponent';
 import { DISHES } from './shared/dishes';
 import Main from './components/MainComponent';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
+
+const store = ConfigureStore();
 
 class App extends Component {
 
   render() {
-    return (  <BrowserRouter>
-      <div className="App">
-        <Main />
-      </div>
-    </BrowserRouter>
+    return (      <Provider store={store}>
+      <BrowserRouter>
+        <div className="App">
+          <Main />
+        </div>
+      </BrowserRouter>
+    </Provider>
     );
   }
 }
